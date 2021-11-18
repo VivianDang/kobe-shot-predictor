e8b6dbf333b0643c46272946a3d4283fea4d6f85
YuanDang <viviandang121@gmail.com>
2021-11-18 13:58:09 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,3 +5,7 @@ import numpy as np
 
 df = pd.read_csv("data.csv")
 # %%
+# drop nan
+df = df.dropna() # na in shot_made_flag
+# drop columns that don't give information
+df = df.drop(["team_id", "team_name"], axis=1)
