13b6e4fc483027489eef555a69e82852ed337df6
Mohammad Maaz <mohammadmaaz1997@gmail.com>
2021-11-29 16:19:32 -0500

    added scaling

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,8 +11,14 @@ df = pd.read_csv("data_cleaned.csv")
 # %%
 
 numerical_features = ['loc_x', 'loc_y', 'minutes_remaining', 'period', 'shot_distance']
-categorical_features = ['combined_shot_type','shot_zone_area', 'shot_zone_basic', 'shot_zone_range', 'opponent']
+categorical_features = ['combined_shot_type', 'shot_zone_area', 'shot_zone_basic', 'shot_zone_range', 'opponent']
 target = 'shot_made_flag'
+
+#%%
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+df[numerical_features] = scaler.fit_transform(df[numerical_features])
+#%%
 
 df = df[numerical_features + categorical_features + [target]]
 
@@ -33,8 +39,8 @@ lr_fit.summary()
 
 #%%
 # get importance
-feature_importance = pd.DataFrame({'feature': lr_fit.params.index, 'coefficient': lr_fit.params.values})
-top_features = feature_importance.sort_values(by=['coefficient'], ascending=False).head(10)
+feature_importance = pd.DataFrame({'feature': lr_fit.params.index, 'coefficient': lr_fit.params.values, 'importance': abs(lr_fit.params.values)})
+top_features = feature_importance.sort_values(by=['importance'], ascending=False).head(10)
 sns.barplot(x='coefficient', y = 'feature', data = top_features)
 # %%
 from sklearn.metrics import accuracy_score, precision_score, recall_score
@@ -48,4 +54,8 @@ print('Recall:', recall_score(Y_test, Y_pred))
 # %%
 from sklearn.metrics import confusion_matrix
 confusion_matrix(Y_test, Y_pred)
+# %%
+
+# %%
+
 # %%
