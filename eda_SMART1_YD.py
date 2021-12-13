434c1855bb372e5ce99518a3298b3c2280b4e6bb
YuanDang <viviandang121@gmail.com>
2021-12-12 22:50:19 -0500

    Update eda_SMART1_YD.py

--- eda_SMART1_YD.py
+++ eda_SMART1_YD.py
@@ -1,43 +1,26 @@
 #%%
 # LIBRARY IMPORTS
-import os
-from matplotlib import colors
-
 import pandas as pd
-import numpy as np
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Rectangle, Arc
 
 from scipy import stats as stats
-import statistics
-import datetime as dt
 
 from scipy.stats import ttest_ind
 from scipy.stats import f_oneway
 
 print("\nIMPORT SUCCESS.")
-
 #%%
 # DATA IMPORTS
-kobe = 'data.csv'
-kobe = pd.read_csv(kobe, header = 0, index_col = 'shot_id')
-kobe.info()
-#kobe.head()
-#kobe.columns
-
-#%%
-# DROP NA VALUES
-kobe = kobe.dropna() # na in shot_made_flag
-
-# DROP IRRELEVANT / REDUNDANT COLUMNS
-kobe_clean = kobe.drop(['team_id', 'team_name', 'game_id', 'game_event_id', 'game_date', 'matchup', 'season'], axis = 1)
+kobe_clean = pd.read_csv('data_cleaned.csv')#, header = 0, index_col = 'shot_id')
 kobe_clean.info()
-#%%
 
+print("\nIMPORT SUCCESS.")
 #%% 
-# draw_court() function from MichaelKrueger's excelent script
+# draw_court() function from MichaelKrueger's excellent script
+# https://www.kaggle.com/bbx396/kobechart
 def draw_court(ax=None, color='black', lw=2, outer_lines=False):
     # If an axes object isn't provided to plot onto, just get current one
     if ax is None:
@@ -106,24 +89,32 @@ def draw_court(ax=None, color='black', lw=2, outer_lines=False):
 
     return ax
 
-
+print("\nReady to continue.")
 #%%
-# define subsets
+############# How does spacial location affect Kobe's shot accuracy? ##############
+# define some useful subsets
+
+# all spatial factors
 kobe_spatial = kobe_clean[["loc_x", "loc_y", "action_type", "combined_shot_type", "shot_zone_area", "shot_type", "shot_zone_basic", "shot_zone_range", "shot_distance", "shot_made_flag"]].copy()
-# kobe_spatial = kobe_spatial[kobe_spatial["action_type"]!=kobe_spatial["combined_shot_type"]]
-kobe_jump = kobe_spatial[kobe_spatial.combined_shot_type=="Jump Shot"]
-kobe_dunk = kobe_spatial[kobe_spatial.combined_shot_type=="Dunk"]
-kobe_layup = kobe_spatial[kobe_spatial.combined_shot_type=="Layup"]
-kobe_tip = kobe_spatial[kobe_spatial.combined_shot_type=="Tip Shot"]
-kobe_hook = kobe_spatial[kobe_spatial.combined_shot_type=="Hook Shot"]
-kobe_bank = kobe_spatial[kobe_spatial.combined_shot_type=="Bank Shot"]
-
+# all action types
 kobe_action = kobe_clean[["action_type", "combined_shot_type", "shot_made_flag"]].copy()
-
 # Short Alias for Action type
 kobe_action.action_type = kobe_action.action_type.apply(lambda x:x.lower().replace(" shot", ""))
 
+# jump shots
+kobe_jump = kobe_spatial[kobe_spatial.combined_shot_type=="Jump Shot"].copy()
+# dunk shot
+kobe_dunk = kobe_spatial[kobe_spatial.combined_shot_type=="Dunk"].copy()
+# layup
+kobe_layup = kobe_spatial[kobe_spatial.combined_shot_type=="Layup"].copy()
+# tip-in
+kobe_tip = kobe_spatial[kobe_spatial.combined_shot_type=="Tip Shot"].copy()
+# hook shot
+kobe_hook = kobe_spatial[kobe_spatial.combined_shot_type=="Hook Shot"].copy()
+# bank shot
+kobe_bank = kobe_spatial[kobe_spatial.combined_shot_type=="Bank Shot"].copy()
 
+print("\nReady to continue.")
 #%%
 ############### Action Type ##################
 ####### Action Type Success Rate #######
@@ -132,7 +123,7 @@ action_accuracy.columns = ["success rate", "count"]
 action_accuracy = action_accuracy.reset_index()
 action_accuracy
 #%%
-#### plot accuracy ####
+####### plot accuracy #######
 fig, ax = plt.subplots(figsize=(9, 6))
 sns.scatterplot(x=action_accuracy.index, y="success rate", size="count", sizes=(5, 1583), size_norm=(1, 15836),data=action_accuracy, alpha=0.7, color="skyblue")
 
@@ -150,170 +141,169 @@ plt.ylabel("Success Rate")
 plt.legend().remove()
 plt.tight_layout()
 plt.show()
-#%%
-
-# fig, ax = plt.subplots(figsize=(9, 6))
-# draw_court(outer_lines=True)
-# sns.scatterplot(x="loc_x", y="loc_y", hue="action_type", style="shot_made_flag", markers={0:"X", 1:"o"}, data=kobe_spatial, alpha=0.7)
-
-# plt.legend(bbox_to_anchor=(1, 1))
-# plt.title("Jump Shot")
-# plt.ylim(-100,500)
-# plt.xlim(300,-300)
-# plt.show()
 # %%
 ############# Combined Shot Type ################
 ####### Combined Shot Type Success Rate #######
 combined_accuracy = kobe_action.groupby("combined_shot_type")["shot_made_flag"].agg(["mean", "count"]).sort_values("mean", ascending=False)
 combined_accuracy.columns = ["success rate", "count"]
-# combined_accuracy = combined_accuracy.reset_index()
 combined_accuracy
 #%%
+# define the shot sides of the basket
 def shot_side(x):
     """Return which side of the basket did kobe shot.
         @param x (pd.Series): row in df
         @return string: {"right side","left side","middle"}
     """
     if x < 0:
-        return "left side"
+        return "leftt side"
     elif x > 0:
         return "right side"
     else: return "middle"
 #%%
-#### jump shot #####
+####### jump shot #######
+## shot range accuracy ##
 jump_accuracy = kobe_jump.groupby("shot_zone_range")["shot_made_flag"].agg(["mean", "count"]).sort_values("mean", ascending=False)
 jump_accuracy.columns = ["success rate", "count"]
-# combined_accuracy = combined_accuracy.reset_index()
 jump_accuracy
 #%%
+## shot side accuracy ##
 kobe_jump["side"] = kobe_jump["loc_x"].apply(lambda x: shot_side(x))
 jump_accuracy2 = kobe_jump.groupby("side")["shot_made_flag"].agg(["mean","count"]).sort_values("mean", ascending=False)
 jump_accuracy2.columns = ["success rate", "count"]
 jump_accuracy2
 #%%
+## F test on shot sides ##
 f_oneway(kobe_jump[kobe_jump['shot_zone_area']=="Left Side(L)"]['shot_made_flag'], kobe_jump[kobe_jump['shot_zone_area']=="Left Side Center(LC)"]['shot_made_flag'],kobe_jump[kobe_jump['shot_zone_area']=="Right Side Center(RC)"]['shot_made_flag'],kobe_jump[kobe_jump['shot_zone_area']=="Right Side(R)"]['shot_made_flag'],kobe_jump[kobe_jump['shot_zone_area']=="Center(C)"]['shot_made_flag'],kobe_jump[kobe_jump['shot_zone_area']=="Back Court(BC)"]['shot_made_flag'])
 #%%
-# plot
+## scatterplot ##
 plt.subplots(figsize=(13,11))
 draw_court(outer_lines=True)
 ax1 = sns.scatterplot(x="loc_x", y="loc_y", hue="shot_made_flag", palette={0:"red",1:"green"}, data=kobe_jump, alpha=0.6)
-# sns.scatterplot(x="loc_x", y="loc_y", hue="shot_zone_basic", style="shot_made_flag", markers={0:"X", 1:"o"}, data=kobe_jump, alpha=0.7)
+
 plt.legend(bbox_to_anchor=(1, 1))
 plt.title("Jump Shot")
 plt.ylim(-100,500)
 plt.xlim(300,-300)
 plt.show()
 # %%
-#### bank shot #####
+####### bank shot #######
+## shot side accuracy ##
 kobe_bank["side"] = kobe_bank["loc_x"].apply(lambda x: shot_side(x))
 bank_accuracy = kobe_bank.groupby("side")["shot_made_flag"].agg(["mean","count"]).sort_values("mean", ascending=False)
 bank_accuracy.columns = ["success rate", "count"]
 bank_accuracy
 #%%
+## T test on sides ##
 ttest_ind(kobe_bank[kobe_bank['loc_x']>0]['shot_made_flag'], kobe_bank[kobe_bank['loc_x']<0]['shot_made_flag'])
 #%%
-# plot
+## scatterplot ##
 plt.subplots(figsize=(8,6))
 draw_court(outer_lines=True)
 ax2 = sns.scatterplot(x="loc_x", y="loc_y", hue="shot_made_flag", palette={0:"red",1:"green"}, data=kobe_bank, alpha=0.7)
 
-# plt.legend(bbox_to_anchor=(1, 1))
 plt.title("Bank Shot")
 plt.ylim(-100,500)
 plt.xlim(300,-300)
 plt.show()
 # %%
-#### hook shot #####
+####### hook shot #######
+## shot side accuracy ##
 kobe_hook["side"] = kobe_hook["loc_x"].apply(lambda x: shot_side(x))
 hook_accuracy = kobe_hook.groupby("side")["shot_made_flag"].agg(["mean","count"]).sort_values("mean", ascending=False)
 hook_accuracy.columns = ["success rate", "count"]
 hook_accuracy
 #%%
+## T test on sides ##
 ttest_ind(kobe_hook[kobe_hook['loc_x']>0]['shot_made_flag'], kobe_hook[kobe_hook['loc_x']<0]['shot_made_flag'])
 #%%
-# plot
+## scatterplot ##
 plt.subplots(figsize=(8,6))
 draw_court(outer_lines=True)
 ax3 = sns.scatterplot(x="loc_x", y="loc_y", hue="shot_made_flag", palette={0:"red",1:"green"}, data=kobe_hook, alpha=0.7)
 
-# plt.legend(bbox_to_anchor=(1, 1))
 plt.title("Hook Shot")
 plt.ylim(-100,500)
 plt.xlim(300,-300)
 plt.show()
 # %%
-#### layup #####
+####### layup #######
+## shot side accuracy ##
 kobe_layup["side"] = kobe_layup["loc_x"].apply(lambda x: shot_side(x))
 layup_accuracy = kobe_layup.groupby("side")["shot_made_flag"].agg(["mean","count"]).sort_values("mean", ascending=False)
 layup_accuracy.columns = ["success rate", "count"]
 layup_accuracy
 #%%
-
+## T test on sides ##
 ttest_ind(kobe_layup[kobe_layup['loc_x']>0]['shot_made_flag'], kobe_layup[kobe_layup['loc_x']<0]['shot_made_flag'])
 #%%
-# plot
+## scatterplot ##
 plt.subplots(figsize=(8,6))
 draw_court(outer_lines=True)
 ax4 = sns.scatterplot(x="loc_x", y="loc_y", hue="shot_made_flag", palette={0:"red",1:"green"}, data=kobe_layup, alpha=0.5)
 
-# plt.legend(bbox_to_anchor=(1, 1))
 plt.title("Layup")
 plt.ylim(-100,500)
 plt.xlim(300,-300)
 plt.show()
 # %%
-#### tip #####   
+####### tip-in #######
+## shot side accuracy ##   
 kobe_tip["side"] = kobe_tip["loc_x"].apply(lambda x: shot_side(x))
 tip_accuracy = kobe_tip.groupby("side")["shot_made_flag"].agg(["mean","count"]).sort_values("mean", ascending=False)
 tip_accuracy.columns = ["success rate", "count"]
 tip_accuracy
 #%%
+## T test on sides ##
 ttest_ind(kobe_tip[kobe_tip['loc_x']>0]['shot_made_flag'], kobe_tip[kobe_tip['loc_x']<0]['shot_made_flag'])
 #%%
-# plot
+## scatterplot ##
 plt.subplots(figsize=(8,6))
 draw_court(outer_lines=True)
 sns.scatterplot(x="loc_x", y="loc_y", hue="shot_made_flag", palette={0:"red",1:"green"}, data=kobe_tip, alpha=0.7)
 
-# plt.legend(bbox_to_anchor=(1, 1))
 plt.title("Tip In")
 plt.ylim(-100,500)
 plt.xlim(300,-300)
 plt.show()
 # %%
-#### dunk shot #####
+####### dunk shot #######
+## shot side accuracy ##
 kobe_dunk["side"] = kobe_dunk["loc_x"].apply(lambda x: shot_side(x))
 dunk_accuracy = kobe_dunk.groupby("side")["shot_made_flag"].agg(["mean","count"]).sort_values("mean", ascending=False)
 dunk_accuracy.columns = ["success rate", "count"]
 dunk_accuracy
 #%%
+## F test on sides ##
 f_oneway(kobe_dunk[kobe_dunk['loc_x']==0]['shot_made_flag'], kobe_dunk[kobe_dunk['loc_x']>0]['shot_made_flag'], kobe_dunk[kobe_dunk['loc_x']<0]['shot_made_flag'])
 #%%
-# plot
+## scatterplot ##
 plt.subplots(figsize=(8,6))
 draw_court(outer_lines=True)
 ax5 = sns.scatterplot(x="loc_x", y="loc_y", hue="shot_made_flag", palette={0:"red",1:"green"}, data=kobe_dunk, alpha=0.7)
 
-# plt.legend(bbox_to_anchor=(1, 1))
 plt.title("Dunk Shot")
 plt.ylim(-100,500)
 plt.xlim(300,-300)
 plt.show()
 #%%[markdown]
-## Action Type Accuracy
-# Dunk shot is the most likely type to succeed.
+## Shot Type Summary
+# Shot side has no significant effect on shot accuracy.  
 #
-# Bank shot and hook shot are very likely to succeed.
-
+# It is very likely to score when Kobe attempts a dunk shot or a bank shot.  
 # %%
 ############# shot_zone_area ################
 ####### Success Rate #######
 area_accuracy = kobe_spatial.groupby("shot_zone_area")["shot_made_flag"].agg(["mean", "count"]).sort_values("mean", ascending=False)
 area_accuracy.columns = ["success rate", "count"]
-# combined_accuracy = combined_accuracy.reset_index()
 area_accuracy
 #%%
-####### Plot #######
+# t test on accuracy in RC and LC
+ttest_ind(kobe_spatial[kobe_spatial['shot_zone_area']=="Right Side Center(RC)"]['shot_made_flag'],kobe_spatial[kobe_spatial['shot_zone_area']=="Left Side Center(LC)"]['shot_made_flag'])
+#%%
+# f test on all areas
+f_oneway(kobe_spatial[kobe_spatial['shot_zone_area']=="Left Side(L)"]['shot_made_flag'], kobe_spatial[kobe_spatial['shot_zone_area']=="Left Side Center(LC)"]['shot_made_flag'],kobe_spatial[kobe_spatial['shot_zone_area']=="Right Side Center(RC)"]['shot_made_flag'],kobe_spatial[kobe_spatial['shot_zone_area']=="Right Side(R)"]['shot_made_flag'],kobe_spatial[kobe_spatial['shot_zone_area']=="Center(C)"]['shot_made_flag'],kobe_spatial[kobe_spatial['shot_zone_area']=="Back Court(BC)"]['shot_made_flag']) # no significant result
+#%%
+####### scatterplot #######
 plt.subplots(figsize=(8,8))
 draw_court(outer_lines=True)
 sns.scatterplot(x="loc_x", y="loc_y", hue="shot_zone_area",style="shot_made_flag", markers={0:"X", 1:"o"}, data=kobe_spatial, alpha=0.7)
@@ -328,10 +318,12 @@ plt.show()
 ####### Success Rate #######
 basic_accuracy = kobe_spatial.groupby("shot_zone_basic")["shot_made_flag"].agg(["mean", "count"]).sort_values("mean", ascending=False)
 basic_accuracy.columns = ["success rate", "count"]
-# combined_accuracy = combined_accuracy.reset_index()
 basic_accuracy
 #%%
-####### Plot #######
+# f test on all areas
+f_oneway(kobe_spatial[kobe_spatial["shot_zone_basic"]=="Restricted Area"]["shot_made_flag"], kobe_spatial[kobe_spatial["shot_zone_basic"]=="In The Paint (Non-RA)"]["shot_made_flag"],kobe_spatial[kobe_spatial["shot_zone_basic"]=="Mid-Range"]["shot_made_flag"],kobe_spatial[kobe_spatial["shot_zone_basic"]=="Left Corner 3"]["shot_made_flag"],kobe_spatial[kobe_spatial["shot_zone_basic"]=="Right Corner 3"]["shot_made_flag"],kobe_spatial[kobe_spatial["shot_zone_basic"]=="Above the Break 3"]["shot_made_flag"]) # no significant result
+#%%
+####### scatterplot #######
 plt.subplots(figsize=(8,8))
 draw_court(outer_lines=True)
 sns.scatterplot(x="loc_x", y="loc_y", hue="shot_zone_basic",style="shot_made_flag", markers={0:"X", 1:"o"}, data=kobe_spatial, alpha=0.7)
@@ -346,10 +338,9 @@ plt.show()
 ####### Success Rate #######
 range_accuracy = kobe_spatial.groupby("shot_zone_range")["shot_made_flag"].agg(["mean", "count"]).sort_values("mean", ascending=False)
 range_accuracy.columns = ["success rate", "count"]
-# combined_accuracy = combined_accuracy.reset_index()
 range_accuracy
 #%%
-####### Plot #######
+####### scatterplot #######
 plt.subplots(figsize=(8,8))
 draw_court(outer_lines=True)
 sns.scatterplot(x="loc_x", y="loc_y", hue="shot_zone_range",style="shot_made_flag", markers={0:"X", 1:"o"}, data=kobe_spatial, alpha=0.7)
@@ -361,7 +352,6 @@ plt.xlim(300,-300)
 plt.show()
 # %%
 ####### Accuracy vs Distance #######
-plt.subplots(figsize=(8,8))
 sns.lmplot(x="shot_distance", y="shot_made_flag",data=kobe_spatial[kobe_spatial.shot_distance < 50],y_jitter=0.02, logistic=True, palette="Blue_r")
 
 plt.title("Accuracy vs Distance")
@@ -369,7 +359,11 @@ plt.xlim(0,55)
 plt.ylabel("Succeed")
 plt.xlabel("Distance(ft)")
 plt.show()
+#%%[markdown]
+## Shot Zone Areas Summary
+# It is clear that the accuracy of Kobe's shots is negatively related with his distance from the hoop. The closer he is to the basket, the more accurate he is. Not surprisingly, every shots shoot from the back court area failed unless some accident happens. Kobe is most accurate when he shoots from the center area.
 # %%
+###### model ######
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -388,7 +382,6 @@ type_dummy = pd.get_dummies(kobe_spa_cp.combined_shot_type, prefix="type")
 kobe_spa_cp = kobe_spa_cp.join([area_dummy, type_dummy])
 kobe_spa_cp.drop(["shot_zone_area", "combined_shot_type"], axis=1, inplace=True)
 #%%
-###### model ######
 model = glm("shot_made_flag~C(shot_zone_area)+shot_distance+C(combined_shot_type)", data=kobe_clean, family=sm.families.Binomial())
 modelfit = model.fit()
 print(modelfit.summary())
@@ -402,7 +395,7 @@ model1.fit(x_train, y_train)
 print('Logit model accuracy (with the test set):', model1.score(x_test, y_test))
 #%%
 # predict
-y_pred = pd.Series(model.predict(X_test))
+y_pred = pd.Series(model.predict(x_test))
 y_test = y_test.reset_index(drop=True)
 z = pd.concat([y_test, y_pred], axis=1)
 z.columns = ['True', 'Prediction']
